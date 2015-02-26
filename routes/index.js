b04503a77ddf16461f01a8025ea5ec9b9ca90308
Chris Finch <chris.finch@guardian.co.uk>
2015-02-26 13:36:13 +0000

    data

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,33 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/quiz', function(req, res, next) {
+    res.send([{
+        id: 0,
+        choices: [{
+            id: 1,
+            text: 'First Choice'
+        },{
+            id: 2,
+            text: 'Second Choice'
+        }]
+        headline: 'My First step',
+        template: '/my/template/path', // Resolve to template
+        related: {} // ?
+    },{
+        id: 1,
+        choices: []
+        headline: 'My second step',
+        template: '/my/template/path', // Resolve to template
+        related: {} // ?
+    },{
+        id: 2,
+        choices: []
+        headline: 'My third step',
+        template: '/my/template/path', // Resolve to template
+        related: {} // ?
+    }]);
+});
+
 module.exports = router;
